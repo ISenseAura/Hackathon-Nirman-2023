3e1c0419aab490d04a90b1ad7881faec971dfa6f
Zerapium <mayurhiwale2451982@gmail.com>
2023-02-15 23:39:58 +0530

    Major change

--- src/components/chatsystem/ChatHome.js
+++ src/components/chatsystem/ChatHome.js
@@ -4,17 +4,44 @@ let ChatHome = () => {
       <section className="section">
         <div className="row">
           <div className="col-lg-3">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Example Card</h5>
-                <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+          <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Chats <span style={{float:"right",borderRadius:"3px",padding:"3px",background:"lightgreen",marginLeft:"0px"}}> New Chat</span></h5>
+          {/* List group with custom content */}
+          <ol className="list-group list-group-numbered">
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">Subheading</div>
+                Cras justo odio
               </div>
-            </div>
+              <span className="badge bg-primary rounded-pill">14</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">Subheading</div>
+                Cras justo odio
+              </div>
+              <span className="badge bg-primary rounded-pill">14</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">Subheading</div>
+                Cras justo odio
+              </div>
+              <span className="badge bg-primary rounded-pill">14</span>
+            </li>
+          </ol>{/* End with custom content */}
+        </div>
+      </div>
           </div>
-          <div className="col-lg-8">
+
+
+
+
+          <div className="col-lg-8" style={{marginRight:"0px"}}>
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Example Card</h5>
+                <h5 className="card-title "><b> Example Card</b></h5>
                 <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
               </div>
             </div>
