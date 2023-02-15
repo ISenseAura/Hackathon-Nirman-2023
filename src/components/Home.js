c2a41addd312c5b9175040c0bb30b32eb99d0ea7
DeltaCoderr <subodhinamdar99@gmail.com>
2023-02-15 05:50:06 +0530

    Beautify

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Navbar from "./Navbar";
-import About from './About';
-import Footer from './Footer';
-import Features from './Features';
-import Cards from './Cards';
+import About from "./About";
+import Footer from "./Footer";
+import Features from "./Features";
 import "./Home.css";
 
 import { useState } from "react";
@@ -13,94 +12,93 @@ let Home = () => {
   let [loginI, setLoginI] = useState(false);
 
   function OnGetStarted() {
-    //let [active,setActive] = useState(false);
-    //document.getElementById("hero").className += " active";
-    document.getElementById("fadeDown").setAttribute("data-aos","fade-down");
-    setLoginI(true)
-   // document.getElementById("fadeDown").style.display = "none";
-    //document.getElementById("fadeIn").style.display = "none";
-   // document.getElementById("fadeIn").setAttribute("data-aos","fade-in");
-   // setActive(!active);
+    document.getElementById("fadeDown").setAttribute("data-aos", "fade-down");
+    setLoginI(true);
   }
 
   return (
     <>
-      { !loginI ?  <section id="hero" class={`hero d-flex align-items-center  ${active ? "active" : ""}`}>
-        <div class="container" id="fadeDown" >
-          <div class="row">
-            <div class="col-xl-4">
-              <h2 data-aos="fade-up">Be A Part Of Our Community</h2>
-              <blockquote data-aos="fade-up" data-aos-delay="100">
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Perspiciatis cum recusandae eum laboriosam voluptatem
-                  repudiandae odio, vel exercitationem officiis provident
-                  minima.{" "}
-                </p>
-              </blockquote>
-              <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
-                <a href="#about" class="btn-get-started" onClick={OnGetStarted}>
-                  Get Started
-                </a>
-                <a
-                  href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
-                  class="glightbox btn-watch-video d-flex align-items-center"
-                >
-                  <i class="bi bi-arrow-down-circle"></i>
-                  <span>Read More</span>
-                </a>
+      {!loginI ? (
+        <section
+          id="hero"
+          class={`hero d-flex align-items-center  ${active ? "active" : ""}`}
+        >
+          <div class="container" id="fadeDown">
+            <div class="row">
+              <div class="col-xl-4">
+                <h2 data-aos="fade-up">Be A Part Of Our Community</h2>
+                <blockquote data-aos="fade-up" data-aos-delay="100">
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Perspiciatis cum recusandae eum laboriosam voluptatem
+                    repudiandae odio, vel exercitationem officiis provident
+                    minima.{" "}
+                  </p>
+                </blockquote>
+                <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
+                  <a
+                    href="#about"
+                    class="btn-get-started"
+                    onClick={OnGetStarted}
+                  >
+                    Get Started
+                  </a>
+                  <a
+                    href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+                    class="glightbox btn-watch-video d-flex align-items-center"
+                  >
+                    <i class="bi bi-arrow-down-circle"></i>
+                    <span>Read More</span>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-
-
-
-          
-        </div>
-
-    
-      </section> : <section id="hero" class={`hero d-flex align-items-center  ${active ? "active" : ""}`}>
-       
-
-        <div class="container"  >
-          <div class="row">
-            <div class="col-xl-4">
-              <h2 data-aos="fade-up">Be A Part Of Our Community 2</h2>
-              <blockquote data-aos="fade-up" data-aos-delay="100">
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Perspiciatis cum recusandae eum laboriosam voluptatem
-                  repudiandae odio, vel exercitationem officiis provident
-                  minima.{" "}
-                </p>
-              </blockquote>
-              <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
-                <a href="#about" class="btn-get-started" onClick={OnGetStarted}>
-                  Get Started
-                </a>
-                <a
-                  href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
-                  class="glightbox btn-watch-video d-flex align-items-center"
-                >
-                  <i class="bi bi-arrow-down-circle"></i>
-                  <span>Read More</span>
-                </a>
+        </section>
+      ) : (
+        <section
+          id="hero"
+          class={`hero d-flex align-items-center  ${active ? "active" : ""}`}
+        >
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-4">
+                <h2 data-aos="fade-up">Be A Part Of Our Community 2</h2>
+                <blockquote data-aos="fade-up" data-aos-delay="100">
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Perspiciatis cum recusandae eum laboriosam voluptatem
+                    repudiandae odio, vel exercitationem officiis provident
+                    minima.{" "}
+                  </p>
+                </blockquote>
+                <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
+                  <a
+                    href="#about"
+                    class="btn-get-started"
+                    onClick={OnGetStarted}
+                  >
+                    Get Started
+                  </a>
+                  <a
+                    href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+                    class="glightbox btn-watch-video d-flex align-items-center"
+                  >
+                    <i class="bi bi-arrow-down-circle"></i>
+                    <span>Read More</span>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
+        </section>
+      )}
 
-
-
-          
-        </div>
-      </section>
-}
-
-<Navbar/>
- <About/>
- <Features/>
- <br/>
- <Footer/>
+      <Navbar />
+      <About />
+      <Features />
+      <br />
+      <Footer />
     </>
   );
 };
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,127 +1,114 @@
-import "./Register.css"
-
+import "./Register.css";
 
 import { useState } from "react";
 
-
 let serverAdd = "http://127.0.0.1:8080";
 let Register = () => {
-
-    const [credentials, setCredentials] = useState({email: "", password: ""}) 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const response = await fetch(serverAdd + "/signup", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({email: credentials.email, password: credentials.password})
-        });
-        const json = await response.json()
-        console.log(json);
-        if (json){
-            // Save the auth token and redirect
-            localStorage.setItem('uid', json.status.uid); 
-         console.log(localStorage.getItem("uid"));
-         
-
-        }
-        else{
-            console.log("Dude!","Invalid credentials");
-        }
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(serverAdd + "/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json) {
+      // Save the auth token and redirect
+      localStorage.setItem("uid", json.status.uid);
+      console.log(localStorage.getItem("uid"));
+    } else {
+      console.log("Dude!", "Invalid credentials");
     }
+  };
 
-    const onChange = (e)=>{
-        setCredentials({...credentials, [e.target.name]: e.target.value})
-    }
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
 
-
-
-    return <div className="rcontainer">
-    <div className="title">Registration</div>
-    <div className="content">
-      <form onSubmit={handleSubmit}>
-        <div className="user-details">
-          <div className="input-box">
-            <span className="details">First Name</span>
-            <input type="text" value={credentials.fname} onChange={onChange} id="fname" name="fname"  placeholder="Enter your First name" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Last Name</span>
-            <input type="text" value={credentials.lname} onChange={onChange} id="lname" name="lname"  placeholder="Enter your Last Name" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Email</span>
-            <input type="text" value={credentials.email} onChange={onChange} id="email" name="email"  placeholder="Enter your email" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Date of Birth</span>
-            <input type="date" value={credentials.dob} onChange={onChange} id="dob" name="dob"   required />
-          </div>
-          <div className="input-box">
-            <span className="details">Password</span>
-            <input type="text" value={credentials.password} onChange={onChange} id="password" name="password"  placeholder="Enter your password" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required />
-          </div>
-        </div>
-        <div className="button">
-          <input type="submit" defaultValue="Next" />
-        </div>
-      </form>
-    </div>
-  </div>
-    
-  
-}
-
-
-
-/*
-
-  
+  return (
     <div className="rcontainer">
-    <div className="title">Registration</div>
-    <div className="content">
-      <form action="#">
-        <div className="user-details">
-          <div className="input-box">
-            <span className="details">College Name</span>
-            <input type="text" placeholder="Enter your College name" required />
+      <div className="title">Registration</div>
+      <div className="content">
+        <form onSubmit={handleSubmit}>
+          <div className="user-details">
+            <div className="input-box">
+              <span className="details">First Name</span>
+              <input
+                type="text"
+                value={credentials.fname}
+                onChange={onChange}
+                id="fname"
+                name="fname"
+                placeholder="Enter your First name"
+                required
+              />
+            </div>
+            <div className="input-box">
+              <span className="details">Last Name</span>
+              <input
+                type="text"
+                value={credentials.lname}
+                onChange={onChange}
+                id="lname"
+                name="lname"
+                placeholder="Enter your Last Name"
+                required
+              />
+            </div>
+            <div className="input-box">
+              <span className="details">Email</span>
+              <input
+                type="text"
+                value={credentials.email}
+                onChange={onChange}
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                required
+              />
+            </div>
+            <div className="input-box">
+              <span className="details">Date of Birth</span>
+              <input
+                type="date"
+                value={credentials.dob}
+                onChange={onChange}
+                id="dob"
+                name="dob"
+                required
+              />
+            </div>
+            <div className="input-box">
+              <span className="details">Password</span>
+              <input
+                type="text"
+                value={credentials.password}
+                onChange={onChange}
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                required
+              />
+            </div>
+            <div className="input-box">
+              <span className="details">Confirm Password</span>
+              <input type="text" placeholder="Confirm your password" required />
+            </div>
           </div>
-          <div className="input-box">
-            <span className="details">Department</span>
-            <input type="text" placeholder="Enter your Department Name" required />
+          <div className="button">
+            <input type="submit" defaultValue="Next" />
           </div>
-          <div className="input-box">
-            <span className="details">Branch</span>
-            <input type="text" placeholder="Enter your Branch" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Year of Graduation</span>
-            <input type="text" placeholder="Enter Year of Graduation" required />
-          </div>
-          <div className="input-box" style={{width: '600px'}}>
-            <span className="details">Address</span>
-            <input type="text" placeholder="Enter your Address" required />
-          </div>
-          <div className="input-box">
-            <span className="details">State</span>
-            <input type="text" placeholder="Enter your State" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Pin Code</span>
-            <input type="text" placeholder="Enter Year of Graduation" required />
-          </div>
-        </div>
-        <div className="button">
-          <input type="submit" defaultValue="Submit" />
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-*/
+  );
+};
 
 export default Register;
